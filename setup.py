6bd05c83fc2252fe081a30cfa2abf8baa6f9a8c5
Keerthi11111111 <keerthana.m.work@gmail.com>
2023-11-12 16:13:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 setup(
     name='DSSS_Homework_2',
     version='0.1.0',
-    description='dsss homework -2 ,
+    description='dsss homework -2',
     author='Keerthana',
     license='Apache',
     # ...
