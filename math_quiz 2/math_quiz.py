ebb8754e7de7687b80e69e84f61c1209f4ccacef
Keerthi11111111 <keerthana.m.work@gmail.com>
2023-11-12 16:48:13 +0100

    update test

--- math_quiz 2/math_quiz.py
+++ math_quiz 2/math_quiz.py
@@ -60,28 +60,3 @@ if __name__ == "__main__":
 
 # have made changes to check the git merge command
     
-import unittest
-from math_quiz import generate_random_integer, generate_random_operator, calculate_result
-
-class TestMathQuizFunctions(unittest.TestCase):
-
-    def test_generate_random_integer(self):
-        result = generate_random_integer(1, 10)
-        self.assertTrue(1 <= result <= 10, "Generated integer is not within the specified range.")
-
-    def test_generate_random_operator(self):
-        result = generate_random_operator()
-        self.assertIn(result, ['+', '-', '*'], "Generated operator is not one of the expected values.")
-
-    def test_calculate_result(self):
-        result = calculate_result(7, 6, '+')
-        self.assertEqual(result, ('7 + 6', 13), "Calculation result is incorrect for addition.")
-
-        result = calculate_result(7, 6, '-')
-        self.assertEqual(result, ('7 - 6', 1), "Calculation result is incorrect for subtraction.")
-
-        result = calculate_result(7, 6, '*')
-        self.assertEqual(result, ('7 * 6', 42), "Calculation result is incorrect for multiplication.")
-
-if __name__ == '__main__':
-    unittest.main()
--- math_quiz 2/tests_math_quiz.py
+++ math_quiz 2/tests_math_quiz.py
@@ -1,30 +1,26 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+import unittest
+from math_quiz import generate_random_integer, generate_random_operator, calculate_result
 
+class TestMathQuizFunctions(unittest.TestCase):
 
-class TestMathGame(unittest.TestCase):
+    def test_generate_random_integer(self):
+        result = generate_random_integer(1, 10)
+        self.assertTrue(1 <= result <= 10, "Generated integer is not within the specified range.")
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+    def test_generate_random_operator(self):
+        result = generate_random_operator()
+        self.assertIn(result, ['+', '-', '*'], "Generated operator is not one of the expected values.")
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_calculate_result(self):
+        result = calculate_result(7, 6, '+')
+        self.assertEqual(result, ('7 + 6', 13), "Calculation result is incorrect for addition.")
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        result = calculate_result(7, 6, '-')
+        self.assertEqual(result, ('7 - 6', 1), "Calculation result is incorrect for subtraction.")
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        result = calculate_result(7, 6, '*')
+        self.assertEqual(result, ('7 * 6', 42), "Calculation result is incorrect for multiplication.")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
